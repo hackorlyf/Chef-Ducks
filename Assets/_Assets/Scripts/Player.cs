8bc9fd1c21c1a92485aba26b1289d854f2f9bbb9
hackorlyf <21bcs055@smvdu.ac.in>
2024-02-09 22:59:41 +0530

    Added lfs tracking for large file

---
+++ Assets/_Assets/Scripts/Player.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] float rotateSpeed = 7f;
+    [SerializeField] GameInput gameInput;
+
+    private bool isWalking;
+    private void Update(){
+        HandleMovement();
+        HandleInteraction();
+    }
+
+    private void HandleInteraction(){
+        
+    }
+
+    private void HandleMovement(){
+        Vector2 inputVector = gameInput.GetMovementInputNormalized();
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = 0.65f;
+        float playerHeight = 2f;
+        bool canMove = !Physics.CapsuleCast(transform.position,transform.position + Vector3.up*playerHeight,  playerRadius, moveDir, moveDistance);
+
+        if(!canMove){
+            //Can't move towards moveDir
+            
+            //Attempt only on X
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0);
+            canMove = !Physics.CapsuleCast(transform.position,transform.position + Vector3.up*playerHeight,  playerRadius, moveDirX, moveDistance);
+
+            if(canMove){
+                //Can move only on the X
+                moveDir = moveDirX;
+            }
+            else{
+                //Can't move only on the x
+
+                //Attempt on the z
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z);
+                canMove = !Physics.CapsuleCast(transform.position,transform.position + Vector3.up*playerHeight,  playerRadius, moveDirZ, moveDistance);
+
+                if(canMove){
+                    //Can move only on z
+                    moveDir = moveDirZ;
+                }
+                else{
+                    //Can't move in any direction
+                }
+            }
+
+            
+        }
+
+        if(canMove){
+            transform.position += moveDir * moveDistance;
+        }
+
+        isWalking = moveDir != Vector3.zero;
+
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+    }
+
+    public bool IsWalking(){
+        return isWalking;
+    }
+}
